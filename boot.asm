6f60631b0b26b1489a529724124ed5c259cbb86b
steveprograming <122278835+steveprograming@users.noreply.github.com>
2024-09-29 13:54:05 +0200

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,24 +1,21 @@
-[ORG 0x7C00]       ; Bootloader bude načten na tuto adresu
-bits 16
-
-jmp start          ; Skoč na začátek bootloaderu
+[ORG 0x7C00]       ; Bootloader bude načten na adresu 0x7C00
+bits 16            ; Pracujeme v 16bitovém režimu
 
 start:
-    ; Vytvoření textového řetězce
-    mov si, msg   ; Načtení adresy zprávy do SI
-    mov ah, 0x0E  ; Funkce pro tisk znaku (BIOS)
+    mov si, msg     ; Načtení adresy zprávy do registru SI
+    mov ah, 0x0E    ; BIOS funkce pro tisk znaku (int 0x10, funkce 0x0E)
 
 print_loop:
-    lodsb          ; Načti byte ze SI do AL
-    cmp al, 0      ; Zkontroluj, zda je konec řetězce
-    je hang        ; Pokud ano, skákej do hang
-    int 0x10      ; Zavolej BIOS interrupt pro tisk znaku
-    jmp print_loop ; Pokračuj v cyklu
+    lodsb           ; Načti byte ze SI do AL (Load String Byte)
+    cmp al, 0       ; Zkontroluj, zda je konec řetězce (0 značí konec)
+    je hang         ; Pokud ano, skoč do nekonečné smyčky (hang)
+    int 0x10        ; Volej BIOS přerušení pro tisk znaku
+    jmp print_loop  ; Pokračuj ve smyčce, dokud nedojdeme na konec řetězce
 
 hang:
-    jmp hang       ; Nekonečná smyčka
+    jmp hang        ; Nekonečná smyčka pro zastavení systému
 
-msg db 'Hello, World from Filip's OS!', 0 ; Zpráva, která se má zobrazit
+msg db 'Hello, World from Filip''s OS!', 0  ; Textová zpráva k zobrazení
 
-times 510-($-$$) db 0 ; Vyplnění zbytku disku nulami
-dw 0xAA55              ; Boot signatura
+times 510-($-$$) db 0  ; Vyplň zbytek sektoru nulami (boot sektor má 512 bytů)
+dw 0xAA55              ; Boot signatura (2 poslední bajty, aby BIOS rozpoznal boot sektor)
